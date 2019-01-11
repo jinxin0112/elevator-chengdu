8cd569c653dd78fcba2eda7829d4e190495928d1
jinxin <m18508218948@163.com>
2019-01-11 17:57:40 +0800

    [feat]: Info

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -1,11 +1,117 @@
-import Taro, { Component } from 'react'
+import Taro, { Component } from '@tarojs/taro'
+import { observer, inject } from '@tarojs/mobx'
+import { Swiper, SwiperItem, View, Image, ScrollView, Text } from '@tarojs/components'
+import { AtGrid,AtIcon } from "taro-ui"
+import './index.less'
 
-export default class Info extends Component {
+interface IFItem {
+  data: Array<object>,
+  toggleStar: any
+}
+
+class Item extends Component<IFItem,any> {
+  toggleStar = ()=> {
+    const {Store,data} = this.props;
+    const {index} = data;
+    Store.toggleStar(index)
+  }
   render() {
+    const { data } = this.props;
+    const {title, date, star} = data
     return (
-      <div>
-        Info
-      </div>
+    <View className='at-row news-item'>
+      <View className='at-col at-col-4'>
+        A
+      </View>
+      <View className='at-col at-col-8'>
+        <Text className='news-title'>{title}</Text>
+        <View className='news-date-star'>
+        <View className='news-date'>{date}</View>
+        <View className='news-star' onClick={this.toggleStar}>
+        <AtIcon value={`heart${star==88?'':'-2'}`} size='16' color='#F00'></AtIcon>
+          <span>{star}</span>
+        </View>
+        </View>
+      </View>
+    </View>
+    )
+  }
+}
+@inject('Store')
+@observer
+export default class Info extends Component {
+  state={
+    autoplay: false
+  }
+  componentDidMount() {
+    this.setState({
+      autoplay: true
+    })
+  }
+  handleScrollToLower = ()=> {
+      console.log('到底了');
+  }
+  render() {
+    console.log('render');
+    const {Store} = this.props;
+    const {news} = Store;
+    const {autoplay} = this.state;
+    return (
+      <ScrollView
+        scrollY
+        onScrollToLower={this.handleScrollToLower}
+      >
+        <Swiper
+          className='swiper-info'
+          indicatorColor='#808891'
+          indicatorActiveColor='#fff'
+          circular
+          indicatorDots
+          autoplay={autoplay}
+        >
+          <SwiperItem>
+            <Image className='image-0' src={require('../../assets/img/swiper0.png')} />
+          </SwiperItem>
+          <SwiperItem>
+            <Image className='image-1' src={require('../../assets/img/swiper0.png')} />
+          </SwiperItem>
+          <SwiperItem>
+            <Image className='image-2' src={require('../../assets/img/swiper0.png')} />
+          </SwiperItem>
+        </Swiper>
+        <View className="grid-box">
+          <AtGrid hasBorder={false} columnNum={4} data={
+            [
+              {
+                image: require('../../assets/img/grid1.png'),
+                value: '法律法规'
+              },
+              {
+                image: require('../../assets/img/grid2.png'),
+                value: '最新资讯'
+              },
+              {
+                image: require('../../assets/img/grid3.png'),
+                value: '最新政策'
+              },
+              {
+                image: require('../../assets/img/grid4.png'),
+                value: '企业名单'
+              }
+            ]
+          } />
+        </View>
+
+        <View className='panel'>
+          <View className='panel__title'>新闻资讯</View>
+          <View className='panel__content'>
+              {
+                news.map((item,index)=><Item key={index} data={item} Store={Store}/>)
+              }
+          </View>
+        </View>
+
+      </ScrollView>
     )
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,33 @@ import { observable, action } from 'mobx'
 
 const Store = observable({
   currentTab: 0,
+  news:[
+    {
+      index: 0,
+      img: '',
+      title: 'E电梯获3000万元A轮融资瞄准1000亿电梯更新改造...',
+      date: '2019-01-01',
+      star: 88
+    },
+    {
+      index: 1,
+      img: '',
+      title: 'E电梯获3000万元A轮融资瞄准1000亿电梯更新改造...',
+      date: '2019-01-01',
+      star: 88
+    }    
+  ],
   changeTab(index){
     this.currentTab = index;
+  }
+  toggleStar(i) {
+    this.news = this.news.map((item)=>{
+      const {star,index} = item;
+      return {
+        ...item,
+        star: star===88&&index===i ? 89: 88
+      }
+    })
   }
 }, {
   changeTab: action
